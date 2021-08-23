42e40fbac50b87aca2165fc81fef3d757fbffcc5
Nouman Amir <noumanamir453@gmail.com>
2021-08-23 19:32:16 +0500

    Added infinix to postfix

--- Array_Based_Stack/main.cpp
+++ Array_Based_Stack/main.cpp
@@ -3,8 +3,12 @@
 #include <string>
 
 bool checkBraces(); // application
+void infinixToPostfix(); //application
+int getPrecedence(char); //utility function
 
 int main (){
+    //checkBraces();
+    //infinixToPostfix();
     std::cin.get();
     return 1;
 }
@@ -38,4 +42,69 @@ bool checkBraces() {
     }
 
     return false;
+}
+
+// Application of Stack
+void infinixToPostfix() {
+    Stack<char> stack;
+    std::string infinix = "{2*(430+10)}^3";
+    std::string postfix = "";
+
+    for(int i = 0; i < infinix.size(); ++i){
+        char c = infinix[i];
+        if (isalnum(c)){
+            postfix+=c;
+        }
+        else if(c == '^'){
+            stack.push(c);
+            postfix+=" ";
+        }
+        else if(c == '(' || c == '[' || c == '{'){
+            stack.push(c);
+        }
+        else if(c == ')' || c == ']' || c == '}'){
+            char temp = stack.pop();
+            while (temp != '(' && temp != '{' && temp != '[' && !stack.isEmpty()){
+                postfix+=temp;
+                temp = stack.pop();
+            }
+        }
+        else {
+            postfix+=' ';
+            if (getPrecedence(c) > getPrecedence(stack.peek())){
+                stack.push(c);
+            }
+            else if(getPrecedence(c) <= getPrecedence(stack.peek())){
+                char temp = stack.pop();
+                if (stack.isEmpty()){
+                    postfix+=temp;
+                }
+                while (temp != '(' && temp != '{' && temp != '[' && !stack.isEmpty()){
+                    postfix+=temp;
+                    temp = stack.pop();
+                }
+                stack.push(c);   
+            }
+        }
+    }
+
+    while(!stack.isEmpty()){
+        postfix+=stack.pop();
+    }
+    std::cout << postfix;
+}
+
+int getPrecedence(char c) {
+    if (c == '+' || c == '-'){
+        return 0;
+    }
+    else if(c == '/' || c == '*'){
+        return 1;
+    }
+    else if(c == '^'){
+        return 2;
+    }
+    else {
+        return -1;
+    }
 }
