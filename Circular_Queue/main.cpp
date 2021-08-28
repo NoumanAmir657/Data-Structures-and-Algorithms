fcbcd151c1fa4f69713f21440cfadaa5f167a6ac
Nouman Amir <noumanamir453@gmail.com>
2021-08-28 15:13:30 +0500

    Added circular queue

--- Circular_Queue/main.cpp
+++ Circular_Queue/main.cpp
@@ -3,17 +3,8 @@
 
 int main() {
     CircularQueue<int> q;
-    q.enqueue(1);
-    q.enqueue(2);
-    q.enqueue(3);
-    q.enqueue(4);
-    q.enqueue(5);
 
-    q.dequeue();
-    q.enqueue(11);
-    q.enqueue(12);
-
-    std::cout << q.firstElement() << '\n';
+    // std::cout << q.firstElement() << '\n';
 
     std::cin.get();
     return 1;
