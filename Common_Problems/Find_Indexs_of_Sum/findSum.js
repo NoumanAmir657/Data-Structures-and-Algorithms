f127fdf89ea93edc124ceecb89f2c4d4bc2c9a11
Nouman Amir <noumanamir453@gmail.com>
2021-09-01 17:28:22 +0500

    Added twoSum optimized solution

--- Common_Problems/Find_Indexs_of_Sum/findSum.js
+++ Common_Problems/Find_Indexs_of_Sum/findSum.js
@@ -1,5 +1,7 @@
 // Time Complexity O(n^2)
 // Space Complexity O(1)
+
+// brute force solution
 var twoSum = (nums, target) => {
     for (let i = 0; i < nums.length; ++i){
         const numberToFind = target - nums[i];
@@ -13,4 +15,26 @@ var twoSum = (nums, target) => {
     return null;
 };
 
+// optimized solution
+
+// Time Complexity O(n)
+// Space Complexity O(n)
+var optimizedTwoSum = (nums, target) => {
+    const numsMap = {};
+    
+    for (let i = 0; i < nums.length; ++i){
+        const currentMapValue = numsMap[nums[i]]
+
+        if (currentMapValue >= 0){
+            return [currentMapValue, i]
+        }
+        else {
+            const numberToFind = target - nums[i];
+            numsMap[numberToFind] = i
+        }
+    }
+    return null
+}
+
 console.log(twoSum([1,3,7,9,2], 11))
+console.log(optimizedTwoSum([1,3,7,9,2], 11))
