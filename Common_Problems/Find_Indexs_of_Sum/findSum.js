5ebd022aed657e9165cd4a5be9898b543346b9cd
Nouman Amir <noumanamir453@gmail.com>
2021-09-01 16:35:46 +0500

    Added findIndexsOfSum brute force solution

---
+++ Common_Problems/Find_Indexs_of_Sum/findSum.js
@@ -0,0 +1,14 @@
+var twoSum = (nums, target) => {
+    for (let i = 0; i < nums.length; ++i){
+        const numberToFind = target - nums[i];
+
+        for (let j = i+1; j < nums.length; ++j){
+            if (numberToFind === nums[j]){
+                return [i, j];
+            }
+        }
+    }
+    return null;
+};
+
+console.log(twoSum([1,3,7,9,2], 11))
