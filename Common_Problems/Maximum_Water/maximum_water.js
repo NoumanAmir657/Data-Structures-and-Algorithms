d8523575848a3a89e7e32de44e4ff4b9620f9951
Nouman Amir <noumanamir453@gmail.com>
2021-09-01 22:21:46 +0500

    Added maximum water optimized solution

--- Common_Problems/Maximum_Water/maximum_water.js
+++ Common_Problems/Maximum_Water/maximum_water.js
@@ -1,3 +1,6 @@
+// Time complexity O(n^2)
+// Space complexity O(1)
+// brute force
 const maximumWater = (array) => {
     let maxArea = 0;
 
@@ -10,4 +13,25 @@ const maximumWater = (array) => {
     return maxArea;
 }
 
-console.log(maximumWater([7,1,2,3,9]));
+// Time complexity O(n)
+// Space complexity O(1)
+// optimized solution
+const optimizedMaximumWater = (array) => {
+    let maxArea = 0;
+    let a = 0;
+    let b = array.length - 1;
+
+    for (let i = 0; i < array.length-1; ++i){
+        let area = Math.min(array[a], array[b]) * (b - a);
+        maxArea = Math.max(area, maxArea);
+        if (a < b){
+            ++a;
+        }
+        else {
+            ++b;
+        }
+    }
+    return maxArea
+}
+
+console.log(optimizedMaximumWater([4,1,8,2,3,9]));
