a9b61bb9d4b6b23ac34c66399ad34f319617dfeb
Nouman Amir <noumanamir453@gmail.com>
2021-09-02 12:46:28 +0500

    Started BST

---
+++ Binary_Search_Trees/bst.h
@@ -0,0 +1,97 @@
+#include <queue>
+#include <stack>
+
+#ifndef BINARY_SEARCH_TREE
+#define BINARY_SEARCH_TREE
+
+template<class T>
+class Stack : public stack<T> {
+    public:
+        T pop() {
+            T temp = top();
+            stack<T>::pop();
+            return temp;
+        }
+};
+
+template<class T>
+class Queue : public queue<T> {
+    public:
+        T dequeue() {
+            T temp = front();
+            Queue<T>::pop();
+            return temp;
+        }
+        void enqueue(const T& value){
+            push(value);
+        }
+};
+
+template<class T>
+class Node {
+    public:
+        Node() {
+            left = 0;
+            right = 0;
+        }
+        Node(const T& e, Node<T> *l = 0, Node<T> *r = 0){
+            info = e;
+            left = l;
+            right = r;
+        }
+        T info;
+        Node<T> *left, *right;
+};
+
+template<class T>
+class BST {
+    public:
+        BST() {
+            root = 0;
+        }
+        ~BST() {
+            clear();
+        }
+        void clear(){
+            clear(root);
+            root = 0;
+        }
+        bool isEmpty() const {
+            return root = 0;
+        }
+        void preorder() {
+            preorder(root);
+        }
+        void inorder() {
+            inorder(root);
+        }
+        void postorder() {
+            postorder(root);
+        }
+        T* search(const T& value) const {
+            return search(root, value);
+        }
+        void breadthFirst();
+        void iterativePreorder();
+        void iterativeInorder();
+        void iterativePostOrder();
+        void MorrisInorder();
+        void insert(const T&);
+        void deleteByMerging(Node<T>*&);
+        void findAndDeleteByMerging(Node<T>*&);
+        void deleteByCopying(Node<T>*&);
+        void balance(T*, int, int);
+
+    protected:
+        Node<T> *root;
+        void clear(Node<T>*);
+        T* search(Node<T>*, const T&) const;
+        void preorder(Node<T>*);
+        void inorder(Node<T>*);
+        void postorder(Node<T>*);
+        virtual void visit(Node<T>* p){
+            cout << p->info << ' ';
+        }
+};
+
+#endif
