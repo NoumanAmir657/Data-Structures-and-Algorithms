25817c7b2b9f75b53479f89faa269753ee494d5e
Nouman Amir <noumanamir453@gmail.com>
2021-09-04 16:14:13 +0500

    Added optimized solution of trapped water

--- Common_Problems/Water_Bars/maxWater.js
+++ Common_Problems/Water_Bars/maxWater.js
@@ -1,6 +1,6 @@
 // Time complexity O(n^2)
 // Space Complexity O(1)
-// brute force solution
+// Brute force solution
 const findMaxWater = (array) => {
     let totalWater = 0;
     let maxR = 0;
@@ -23,5 +23,40 @@ const findMaxWater = (array) => {
     return totalWater;
 }
 
-console.log(findMaxWater([0,1,0,2,1,0,3,1,0,1,2]))
-console.log(findMaxWater([5,0,3,0,0,0,2,3,4,2,1]))
+// Time Complexity O(n)
+// Space Complexity O(1)
+// Optimized solution
+const optimizedFindMaxWater = (array) => {
+    let totalWater = 0;
+    let maxR = 0; // stores largest value on right hand side of array
+    let maxL = 0; //stores largest value on left hand side of array
+    let pL = 0; // left pointer
+    let pR = array.length; // right pointer
+
+    while (pL !== pR){
+        // left pointer has value which is less than or equal to right pointer value
+        if (array[pL] <= array[pR]){
+            if (array[pL] <= maxL){
+                totalWater = totalWater + (maxL-array[pL]);
+            }
+            else {
+                maxL = array[pL];
+            }
+            ++pL;
+        }
+        // right pointer has value which is less than left pointer value
+        else {
+            if (array[pR] <= maxR){
+                totalWater = totalWater + (maxR-array[pR]);
+            }
+            else {
+                maxR = array[pR];
+            }
+            --pR;
+        }
+    }
+    return totalWater;
+}
+
+console.log(optimizedFindMaxWater([0,1,0,2,1,0,3,1,0,1,2]))
+console.log(optimizedFindMaxWater([5,0,3,0,0,0,2,3,4,2,1]))
