0df30f752fa3705776d6e17de104e9168fd1f213
Nouman Amir <noumanamir453@gmail.com>
2021-09-19 14:15:21 +0500

    Added cycle detection

--- Common_Problems/Cycle_Detection/cycleDetection.js
+++ Common_Problems/Cycle_Detection/cycleDetection.js
@@ -14,3 +14,35 @@ const detectCycle = (head) => {
     }
     return currentNode
 }
+
+// Floyd's Hare and Tortorise Algorithm for Cycle Detection
+// Time Complexity O(n)
+// Space Complexity O(1)
+
+const optimizedDetectCycle = (head) => {
+    let hare = head, tort = head;
+
+    while (true) {
+        hare = hare.next;
+        tort = tort.next;
+
+        if (hare.next === null || hare === null) {
+            return false;
+        }
+        else {
+            hare = hare.next;
+        }
+
+        if (tort === hare) {
+            break
+        }
+    }
+
+    let p1 = head, p2 = tort;
+    while (p1 !== p2){
+        p1 = p1.next
+        p2 = p2.next
+    }
+
+    return p1
+}
