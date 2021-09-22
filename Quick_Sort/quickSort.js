74aa9c0c81c91410c8519bd03deb81c05d3d107f
Nouman Amir <noumanamir453@gmail.com>
2021-09-22 18:56:11 +0500

    Added quicksort

---
+++ Quick_Sort/quickSort.js
@@ -0,0 +1,36 @@
+const quickSort = (array, startIndex, endIndex) => {
+    if (startIndex < endIndex){
+        let pivotIndex = partition(array, startIndex, endIndex)
+
+        quickSort(array, startIndex, pivotIndex-1)
+        quickSort(array, pivotIndex+1, endIndex)
+    }
+}
+
+const partition = (array, startIndex, endIndex) => {
+    let pivot = array[startIndex]
+
+    let middleIndex = startIndex
+
+    for (let i = startIndex+1; i <= endIndex; ++i){
+        if (array[i] < pivot){
+            ++middleIndex
+
+            let temp = array[i]
+            array[i] = array[middleIndex]
+            array[middleIndex] = temp
+        }
+    }
+
+    let temp = array[startIndex]
+    array[startIndex] = array[middleIndex]
+    array[middleIndex] = temp
+
+    return middleIndex
+}
+
+const array = [25,21,12,40,37,43,14,28]
+
+console.log(array)
+quickSort(array, 0, array.length - 1)
+console.log(array)
