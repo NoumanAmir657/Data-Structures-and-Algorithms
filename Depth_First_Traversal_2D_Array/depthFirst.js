dd740a10725ef94b1c4b2e915d2db96f697fb1c5
Nouman Amir <noumanamir453@gmail.com>
2021-10-03 21:29:38 +0500

    Added depth first traversal

---
+++ Depth_First_Traversal_2D_Array/depthFirst.js
@@ -0,0 +1,38 @@
+// Time Complexity O(n)
+// Space Complexity O(n)
+const traversalDFS = (matrix) => {
+    const seen = new Array(matrix.length).fill(0).map(() => new Array(matrix[0].length).fill(false))
+    const values = []
+    
+    DFS(matrix, 0, 0, seen, values)
+    return values
+}
+
+const DFS = (matrix, row, col, seen, values) => {
+    if (row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length || seen[row][col]){
+        return;
+    }
+    values.push(matrix[row][col])
+    seen[row][col] = true
+
+    for (let i = 0; i < directions.length; ++i) {
+        const currentDir = directions[i]
+        DFS(matrix, row+currentDir[0], col+currentDir[1], seen, values)
+    }
+}
+
+const matrix = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15],
+    [16,17,18,19,20],
+]
+
+const directions = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1]
+]
+
+console.log(traversalDFS(matrix))
