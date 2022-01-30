a80a7c9b160575f3567e7584adee74796628fbfe
Nouman Amir <noumanamir453@gmail.com>
2022-01-31 00:45:47 +0500

    added optimized minumim cost of staires

--- Problems/Minimum_Cost_For_Staires/solution.js
+++ Problems/Minimum_Cost_For_Staires/solution.js
@@ -1,10 +1,15 @@
-// const minCostClimbingStaires = (array, index, totalCost) => {
-//     if (index >= array.length){return totalCost}
-//     if (index !== -1){totalCost = totalCost + array[index]}
-//     return Math.min(sol(array, index+1, totalCost), sol(array, index+2, totalCost))
-// }
+//brute force solution
+//Time Complexity O(2^n)
+//Space Complexity O(2^n)
+const bruteMinCostClimbingStaires = (array, index, totalCost) => {
+    if (index >= array.length){return totalCost}
+    if (index !== -1){totalCost = totalCost + array[index]}
+    return Math.min(sol(array, index+1, totalCost), sol(array, index+2, totalCost))
+}
 
 //top-down approach
+//Time Complexity O(n)
+//Space Complexity O(n)
 const minCostClimbingStaires = (array) => {
     const n = array.length
     const dp = new Array(n).fill(-1)
@@ -21,6 +26,8 @@ const minCost = (i, cost, dp) => {
 }
 
 //bottom-up approach
+//Time Complexity O(n)
+//Space Complexity O(n)
 const iterativeMinCostClimbingStaires = (array) => {
     const dp = new Array(array.length).fill(0)
     dp[0] = array[0]
@@ -31,5 +38,29 @@ const iterativeMinCostClimbingStaires = (array) => {
     return Math.min(dp[dp.length-1], dp[dp.length-2])
 }
 
+//bottom-up approach
+//further optimized
+//Time Complexity O(n)
+//Space Complexity O(1)
+const optimizedIterativeMinCostClimbingStaires = (array) => {
+    const dp = new Array(array.length).fill(0)
+    let count = 0
+    dp[0] = array[0]
+    dp[1] = array[1]
+    for (let i= 2; i < array.length; ++i){
+        if (count === 0){
+            dp[0] = array[i] + Math.min(dp[0], dp[1])
+            count = 1
+        }
+        else {
+            dp[1] = array[i] + Math.min(dp[0], dp[1])
+            count = 0
+        }
+        
+    }
+    return Math.min(dp[0], dp[1])
+}
+
 console.log(iterativeMinCostClimbingStaires([1,1,3,5]))
 console.log(minCostClimbingStaires([1,1,3,5]))
+console.log(optimizedIterativeMinCostClimbingStaires([1,1,3,5]))
