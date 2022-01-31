54ef8900a848ae96f17d9299e2889a071e885a31
Nouman Amir <noumanamir453@gmail.com>
2022-01-31 21:26:53 +0500

    top down knight's probability

--- Problems/Knight_Probability/solution.js
+++ Problems/Knight_Probability/solution.js
@@ -41,6 +41,35 @@ const recursiveUtil = (n, k, r, c, dp) => {
     return dp[k][r][c]
 }
 
+//bottom-up
+//Time Complexity O(n^2 * k)
+//Space Complexity O(n^2 * k)
+const bottomUpKnightProbability = (n, k, r, c) => {
+    const dp = new Array(k+1).fill(0).map(() => new Array(n).fill(0).map(() => new Array(n).fill(0)))
+    dp[0][r][c] = 1
+    for (let i = 1; i <= k; ++i){
+        for (let row = 0; row < n; ++row){
+            for(let col = 0; col < n; ++col){
+                for (let d = 0; d < directions.length; ++d){
+                    const dir = directions[d]
+                    const prevRow = row + dir[0]
+                    const prevCol = col + dir[1]
+                    if (prevRow >= 0 && prevRow < n && prevCol >= 0 && prevCol < n){
+                        dp[i][row][col] += dp[i-1][prevRow][prevCol]/8
+                    }
+                }
+            }
+        }
+    }
+    let probabilitySum = 0
+    for (let i = 0; i < n; ++i){
+        for (let j = 0; j < n; ++j){
+            probabilitySum += dp[k][i][j]
+        }
+    }
+    return probabilitySum
+}
+
 
 const directions = [
     [-2,-1], [-2,1], [-1,2], [1,2], 
@@ -49,3 +78,4 @@ const directions = [
 
 console.log(bruteForceKnightProbability(6, 3, 2, 2))
 console.log(topDownKnightProbability(6, 3, 2, 2))
+console.log(bottomUpKnightProbability(6, 3, 2, 2))
